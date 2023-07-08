f808a3fcc3698f87c47e9a2cb5d6a73301e38ca7
Sahil <leviack629@gmail.com>
2023-07-08 22:26:11 +0530

    images

--- src/index.js
+++ src/index.js
@@ -1,34 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 function BookList() {
   return (
-    <div>
-      <h2>Amazon Best Selling Books</h2>
+    <section className="booklist">
       <Book />
       <Book />
       <Book />
-    </div>
+      <Book />
+    </section>
   );
 }
 
 const Book = () => {
   return (
-    <article>
+    <article className="book">
       <Image />
       <Title />
       <Author />
-
     </article>
-  )
-}
+  );
+};
 
-const Image = () => <img src="https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg" alt="Atomic Habits" />
-const Title = () => <h2>Atomic Habits</h2>
+const Image = () => (
+  // <img
+  //   src="https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg"
+  //   alt="Atomic Habits"
+  // />
+  <img src="./images/book.jpg" alt="atomic habits" />
+);
+const Title = () => <h2>Atomic Habits</h2>;
 const Author = () => {
-  return <h4>James Clear</h4>
-}
- 
+  return <h4>James Clear</h4>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<BookList />);
